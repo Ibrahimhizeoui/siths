319136b4fa17301f37a46d023946f11330b22e21
ib92 <ibrahim.hizeoui@byon8.com>
2018-07-17 14:34:50 +0200

    fix siths card

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 const callbackUrl = 'http://localhost:3000/test';
-const ApiHostname = 'https://client-test.grandid.com';
+const ApiHostname = 'https://client.grandid.com';
 const apiKey='73f9b0dac47004556036f77d1b2c743e';
 const authenticateServiceKey='cb8bb8e580e7d9e7d586fe64fd3980b9';
 //const callbackUrl = "";
@@ -13,18 +13,34 @@ router.get('/', function(req, res, next) {
     if (err) res.render('err', { error: err });
     else {
     	let r = JSON.parse(body);
-      console.log('Cookies: ', req.cookies)
       res.redirect(r.redirectUrl);
     }
   });
   //res.render('index', { title: 'Express' });
 });
 
-router.get('/category', function(req, res) {
-  var string = encodeURIComponent('something that would break');
-  res.redirect('/?valid=' + string);
-});
+
 router.get('/test', function(req, res, next) {
-  res.render('test', { req: req });
+  const grandidsession = req.query.grandidsession
+  const url = `${ApiHostname}/json1.1/GetSession?apiKey=${apiKey}e&authenticateServiceKey=${authenticateServiceKey}&sessionId=${grandidsession}`
+  request(url, (err, result, body) => {
+    if (err) res.render('err', { error: err });
+    else {
+      let r = JSON.parse(body);
+      res.render('test', { provier: r });
+    }
+  });
+
+});
+
+router.get('/logout', function(req, res, next) { 
+  const url = `${ApiHostname}/json1.1/Logout`;
+  request(url, (err, result, body) => {
+    if (err) res.render('err', { error: err });
+    else {
+      let r = JSON.parse(body);
+      console.log(r);
+    }
+  });
 });
 module.exports = router;
